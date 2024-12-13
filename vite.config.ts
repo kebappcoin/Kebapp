e757912bd78b5d2f0267c1fe54518a3430f5d128
kiranechor <123799085+kiranechor@users.noreply.github.com>
2024-12-14 01:46:42 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/kebapp/', // Add this line for GitHub Pages
+  base: '/Kebapp/',  // Must match your repository name exactly
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
